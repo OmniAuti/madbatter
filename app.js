6c6e6603aa16858170ed4ed4c551df7f43b2a487
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-03-01 22:14:09 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+const nav = document.querySelector('nav')
+const hr = document.querySelector('hr')
 
+
+window.addEventListener('scroll', () => {
+    const navTop = nav.getBoundingClientRect()
+   if (navTop.top === 0) {
+       hr.style.display = 'none'
+       nav.style.borderColor = '#000'
+    } else {
+        hr.style.display = 'block'
+        nav.style.borderColor = 'transparent'
+   }
+})
 
